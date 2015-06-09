c8eb66ad4dea71c0f0d25b11e0dc67aa9b596cd4
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-09 01:44:26 -0400

    example plugin usage

--- example.js
+++ example.js
@@ -2,14 +2,22 @@ var fs = require('fs');
 var prettify = require('./');
 var Remarkable = require('remarkable');
 
+
+function pretty(str, options) {
+  return new Remarkable()
+    .use(prettify)
+    .render(str);
+}
+
 function read(name) {
   return fs.readFileSync('fixtures/' + name + '.md', 'utf8');
 }
-var md = new Remarkable();
-md.use(prettify);
-var result = md.render('\n\n\n# foo\n\n\nbar\n# baz');
-console.log(result);
+
+// var md = new Remarkable();
+// md.use(prettify);
+// var result = md.render('\n\n\n# foo\n\n\nbar\n# baz');
+
 // console.log(pretty(read('example')));
-// console.log(pretty(read('blockquotes')));
-// console.log(pretty(read('emphasis')));
-// console.log(pretty(read('table')));
+console.log(pretty(read('blockquotes')));
+console.log(pretty(read('emphasis')));
+console.log(pretty(read('table')));
