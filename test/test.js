fdb11c259cc90a467465e10a8ee5ac7423393d42
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-21 19:07:57 -0400

    move tests/fixtures

--- test/test.js
+++ test/test.js
@@ -1,18 +1,10 @@
-/*!
- * prettify-markdown <https://github.com/jonschlinkert/prettify-markdown>
- *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
- */
-
 'use strict';
 
-/* deps:mocha */
+require('mocha');
 var fs = require('fs');
 var assert = require('assert');
-var should = require('should');
 var Remarkable = require('remarkable');
-var prettify = require('./');
+var prettify = require('..');
 
 /**
  * test utils
@@ -26,85 +18,84 @@ function read(dir, name) {
   return fs.readFileSync(dir + '/' + name + '.md', 'utf8');
 }
 function fixture(name) {
-  return read('fixtures', name);
+  return read('test/fixtures', name);
 }
 function expected(name) {
-  return read('expected', name);
+  return read('test/expected', name);
 }
-
 
 describe('prettify', function () {
   describe('newlines', function () {
     it('should strip leading newlines:', function () {
-      pretty('\n\n\nb').should.equal('b');
+      assert.equal(pretty('\n\n\nb'), 'b');
     });
 
     it('should strip trailing newlines:', function () {
-      pretty('b\n\n\n').should.equal('b');
+      assert.equal(pretty('b\n\n\n'), 'b');
     });
 
     it('should reduce multiple newlines down to two:', function () {
-      pretty('foo\n\n\nbar').should.equal('foo\n\nbar');
-      pretty('a\n\n\nb').should.equal('a\n\nb');
-      pretty('a\n\n\n\n\n\n\nb').should.equal('a\n\nb');
+      assert.equal(pretty('foo\n\n\nbar'), 'foo\n\nbar');
+      assert.equal(pretty('a\n\n\nb'), 'a\n\nb');
+      assert.equal(pretty('a\n\n\n\n\n\n\nb'), 'a\n\nb');
     });
 
     it('should format headings to have two newlines before content:', function () {
-      pretty('# foo\nbar').should.equal('# foo\n\nbar');
-      pretty('## foo\n\n\nbar').should.equal('## foo\n\nbar');
+      assert.equal(pretty('# foo\nbar'), '# foo\n\nbar');
+      assert.equal(pretty('## foo\n\n\nbar'), '## foo\n\nbar');
     });
 
     it('should ensure an extra newline before and after gfm code blocks:', function () {
       var str = 'a\n```js\nvar foo = "bar";\n```\nb';
-      pretty(str).should.equal('a\n\n```js\nvar foo = "bar";\n```\n\nb');
+      assert.equal(pretty(str), 'a\n\n```js\nvar foo = "bar";\n```\n\nb');
     });
 
     // see: https://github.com/jonschlinkert/pretty-remarkable/issues/1
     it('should properly handle undefined language after code fence:', function () {
       var str = 'a\n```\nvar foo = "bar";\n```\nb';
-      pretty(str).should.equal('a\n\n```\nvar foo = "bar";\n```\n\nb');
+      assert.equal(pretty(str), 'a\n\n```\nvar foo = "bar";\n```\n\nb');
     });
 
     it('should remove leading lines inside gfm code blocks:', function () {
       var str = 'a\n```\n\n\n     var foo = "bar";\n```\nb';
-      pretty(str).should.equal('a\n\n```\n     var foo = "bar";\n```\n\nb');
+      assert.equal(pretty(str), 'a\n\n```\n     var foo = "bar";\n```\n\nb');
     });
 
     // see: https://github.com/jonschlinkert/pretty-remarkable/issues/2
     it('should not remove leading whitespace on the first gfm code line:', function () {
       var str = 'a\n```\n     var foo = "bar";\n```\nb';
-      pretty(str).should.equal('a\n\n```\n     var foo = "bar";\n```\n\nb');
+      assert.equal(pretty(str), 'a\n\n```\n     var foo = "bar";\n```\n\nb');
     });
   });
 
   describe('strong', function () {
     it('should give "section headings" headings two newlines before content:', function () {
-      pretty('foo\n**foo**\nbar').should.equal('foo\n\n**foo**\n\nbar');
-      pretty('foo\n\n**foo**\nbar').should.equal('foo\n\n**foo**\n\nbar');
-      pretty('**foo**\nbar').should.equal('**foo**\n\nbar');
-      pretty('**foo**\n\n\nbar').should.equal('**foo**\n\nbar');
+      assert.equal(pretty('foo\n**foo**\nbar'), 'foo\n\n**foo**\n\nbar');
+      assert.equal(pretty('foo\n\n**foo**\nbar'), 'foo\n\n**foo**\n\nbar');
+      assert.equal(pretty('**foo**\nbar'), '**foo**\n\nbar');
+      assert.equal(pretty('**foo**\n\n\nbar'), '**foo**\n\nbar');
     });
 
     it('should not reformat bolded text that is not a heading:', function () {
-      pretty('a b c **foo**\nbar').should.equal('a b c **foo**\nbar');
-      pretty('one **two** three\nbar').should.equal('one **two** three\nbar');
+      assert.equal(pretty('a b c **foo**\nbar'), 'a b c **foo**\nbar');
+      assert.equal(pretty('one **two** three\nbar'), 'one **two** three\nbar');
     });
   });
 
   describe('badges', function () {
     it('should format badges:', function () {
       var str = '[![foo](https://a.b.c.svg)](http://a.b.c)';
-      pretty(str).should.equal('[![foo](https://a.b.c.svg)](http://a.b.c)');
+      assert.equal(pretty(str), '[![foo](https://a.b.c.svg)](http://a.b.c)');
     });
   });
 
   describe('lists', function () {
     it('should format unordered lists:', function () {
-      pretty(fixture('ul')).should.equal(expected('ul'));
+      assert.equal(pretty(fixture('ul')), expected('ul'));
     });
 
     it('should format ordered lists:', function () {
-      pretty(fixture('ol')).should.equal(expected('ol'));
+      assert.equal(pretty(fixture('ol')), expected('ol'));
     });
   });
 });
