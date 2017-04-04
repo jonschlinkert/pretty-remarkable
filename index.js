d675a6717a96230d5b8b0d5cef308fa3d23af22f
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-04 03:49:44 -0400

    run udpate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * pretty-remarkable <https://github.com/jonschlinkert/pretty-remarkable>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
